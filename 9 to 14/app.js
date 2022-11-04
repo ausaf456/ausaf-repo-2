46913e7f695a1b3d12d85ec2246d457abb79ed31
ausaf456 <ausafshah456@gmail.com>
2022-11-04 23:28:15 +0500

    done

---
+++ 9 to 14/app.js
@@ -0,0 +1,90 @@
+//chapter 9
+// var name =prompt("Your Name");
+// var phone  =prompt("phone");
+// var age =prompt("Age");
+// var Qualification =prompt("Your Qualification");
+// var address =prompt("Your Address");
+
+// console.log(name);
+// console.log(phone);
+// console.log(age);
+// console.log(Qualification);
+// console.log(address);
+
+//Chapter 10
+// var capital=prompt("capital of pakistan")
+// if(capital == "islamabad"){
+//     console.log("correct");
+// }
+
+//chapter11
+// var value = prompt("please enter the value");
+// if(value > 10){
+// console.log("Greater");
+// }
+// else if(value < 10){
+//     console.log("lesser");
+// }
+//chapter 12
+
+
+// var name = +prompt("Your Name");
+// var math = +prompt("Maths marks");
+// var chemistry = +prompt("Chemistry marks");
+// var physics = +prompt("Physics matks");
+// var english = +prompt("English marks");
+// var urdu = +prompt("Urdu marks");
+
+
+// console.log(name);
+// console.log(math);
+// console.log(chemistry);
+// console.log(physics);
+// console.log(english);
+// console.log(urdu);
+ 
+//  var total = math+chemistry+physics+english+urdu;
+//  console.log(total);
+
+
+//  var totalvalue =500;
+//  var result = total/totalvalue *100;
+ 
+ 
+// if(result <=100 && result>=90){
+//     alert("A+")
+// }
+// else if(result <=90 && result>=80){
+//     alert("A+")
+// }
+// else if(result <=80 && result>=70){
+//     alert("A")
+// }
+// else if(result <=70 && result>=60){
+//     alert("B")
+// }
+// else if(result <=60 && result>=50){
+//     alert("C")
+// }
+// else if(result <=50 && result>=40){
+//     alert("D")
+// }
+// else if(result <=40 && result>=30){
+//     alert("Fail")
+// }
+// else{"you entered incorrect value"
+// }
+
+
+// chapter 13
+var age = prompt("your age");
+
+console.log(age)
+
+if( age<=30 && age>=8  ){
+    alert("Allow")
+}
+else{
+    alert("not allow")
+}
+
